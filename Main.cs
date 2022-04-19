7b244b106cd5cc0e9d353451998fae0a0a5c261c
DarigRul <dar9k9@hotmail.com>
2022-04-19 14:58:24 -0500

    CatalogoX

--- Main.cs
+++ Main.cs
@@ -756,5 +756,21 @@ namespace ALTIMA_ERP_2022
                 frm.Show();
             }
         }
+
+        private void buttonItem2_Click(object sender, EventArgs e)
+        {
+            Form frm = this.MdiChildren.Cast<Form>().FirstOrDefault(x => x is CatalogoX.CatalogoX);
+            if (frm != null)
+            {
+                frm.BringToFront();
+                return;
+            }
+            else
+            {
+                frm = new CatalogoX.CatalogoX();
+                frm.MdiParent = this;
+                frm.Show();
+            }
+        }
     }
 }
